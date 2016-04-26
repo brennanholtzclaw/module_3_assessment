160022377960b4d9df607aa431ef3f7f66df89ca
Brennan Holtzclaw <brennanholtzclaw@gmail.com>
2016-04-26 09:50:12 -0600

    Add Show view and pass second line of user story

--- spec/api/v1/items/items_request_spec.rb
+++ spec/api/v1/items/items_request_spec.rb
@@ -1,19 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe "a GET request to /api/v1/items" do
-  it "returns a 200 JSON response" do
-
+  it "returns a 200 JSON response and all items" do
     item1 = Item.create(name: "test name", description: "test description", image_url: "http://www.example.com/test_image.png")
+    item2 = Item.create(name: "another test name", description: "another test description", image_url: "http://www.example.com/another_test_image.png")
+    item3 = Item.create(name: "final test name", description: "final test description", image_url: "http://www.example.com/final_test_image.png")
 
     get "api/v1/items"
 
-    # expect(response.is_json).to eq true
     expect(response.status).to eq 200
     expect(parsed_response[0]["name"]).to eq item1.name
     expect(parsed_response[0]["description"]).to eq item1.description
     expect(parsed_response[0]["image_url"]).to eq item1.image_url
     expect(parsed_response[0]["created_at"]).to eq nil
     expect(parsed_response[0]["updated_at"]).to eq nil
+  end
+
+  it "returns a 200 JSON response and a single item" do
+    item1 = Item.create(name: "test name", description: "test description", image_url: "http://www.example.com/test_image.png")
+    item2 = Item.create(name: "another test name", description: "another test description", image_url: "http://www.example.com/another_test_image.png")
+    item3 = Item.create(name: "final test name", description: "final test description", image_url: "http://www.example.com/final_test_image.png")
+
+    get "api/v1/items/1"
+
+    expect(response.status).to eq 200
+    expect(parsed_response["name"]).to eq item1.name
+    expect(parsed_response["description"]).to eq item1.description
+    expect(parsed_response["image_url"]).to eq item1.image_url
+    expect(parsed_response["created_at"]).to eq nil
+    expect(parsed_response["updated_at"]).to eq nil
+    expect(parsed_response).to_not include(item2)
+    expect(parsed_response).to_not include(item3)
   end
 end
 
