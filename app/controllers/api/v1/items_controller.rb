87aec746d62efea9861d4c3da33d4228dcc4c8ee
Brennan Holtzclaw <brennanholtzclaw@gmail.com>
2016-04-26 10:05:52 -0600

    Passing params to POST request to create new item

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -8,4 +8,13 @@ class Api::V1::ItemsController < ApiController
   def show
     respond_with Item.find(params[:id])
   end
+
+  def destroy
+    respond_with Item.destroy(params[:id])
+  end
+
+  def create
+    binding.pry
+    respond_with Item.create_by(params)
+  end
 end
--- spec/api/v1/items/items_request_spec.rb
+++ spec/api/v1/items/items_request_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe "a GET request to /api/v1/items" do
+RSpec.describe "a request is sent to /api/v1/items" do
   it "returns a 200 JSON response and all items" do
     item1 = Item.create(name: "test name", description: "test description", image_url: "http://www.example.com/test_image.png")
     item2 = Item.create(name: "another test name", description: "another test description", image_url: "http://www.example.com/another_test_image.png")
     item3 = Item.create(name: "final test name", description: "final test description", image_url: "http://www.example.com/final_test_image.png")
 
-    get "api/v1/items"
+    get "/api/v1/items"
 
     expect(response.status).to eq 200
     expect(parsed_response[0]["name"]).to eq item1.name
@@ -21,7 +21,7 @@ RSpec.describe "a GET request to /api/v1/items" do
     item2 = Item.create(name: "another test name", description: "another test description", image_url: "http://www.example.com/another_test_image.png")
     item3 = Item.create(name: "final test name", description: "final test description", image_url: "http://www.example.com/final_test_image.png")
 
-    get "api/v1/items/1"
+    get "/api/v1/items/1"
 
     expect(response.status).to eq 200
     expect(parsed_response["name"]).to eq item1.name
@@ -31,6 +31,25 @@ RSpec.describe "a GET request to /api/v1/items" do
     expect(parsed_response["updated_at"]).to eq nil
     expect(parsed_response).to_not include(item2)
     expect(parsed_response).to_not include(item3)
+  end
+
+  it "returns a 204 response and destroys a single item" do
+    # Apparently there are two identical items being made before each test...?
+    # Where are the two mysterious items coming from?
+
+    delete "api/v1/items/1"
+
+    expect(response.status).to eq 204
+    expect(Item.count).to eq 1
+  end
+
+  it "returns a 201 JSON response and single new item" do
+    new_item = {name: "new item", description: "new description", image_url: "http://www.example.com/new_image.png"}
+    
+    post "api/v1/items", new_item
+
+    expect(response.status).to eq 201
+    expect(Item.count).to eq 1
   end
 end
 
